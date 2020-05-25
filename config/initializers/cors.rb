ad107e03acb96d88deff9fd9acfcb0e9116c1f09
franco-canedo <francanedo1@hotmail.com>
2020-05-25 15:22:29 -0500

    cors change

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://eclat-homes.herokuapp.com/'
+    origins '*'
 
     resource '*',
       headers: :any,
