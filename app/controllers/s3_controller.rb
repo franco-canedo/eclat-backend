d91e71edd435e63f576192d13ccaf912de773ece
franco-canedo <francanedo1@hotmail.com>
2020-05-25 16:19:33 -0500

    authorize s3 post

--- app/controllers/s3_controller.rb
+++ app/controllers/s3_controller.rb
@@ -1,4 +1,6 @@
 class S3Controller < ApplicationController
+  skip_before_action :authorized, only: [:direct_post]  
+  
   def direct_post
     data = S3_BUCKET.presigned_post(key: "uploads/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read')
     render json: { url: data.url, fields: data.fields }, status: :ok
